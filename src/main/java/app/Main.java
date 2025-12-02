98f9099b6e1659a546d5a8d21d7a4cba31029baa
Kaitlyn-Z <kaitlynzhu06@gmail.com>
2025-12-02 11:49:53 -0500

    build: Ignore build artifacts and IDE files
    Updates .gitignore to exclude Maven, IntelliJ, and other generated files. Removes previously tracked build artifacts and IDE configuration files from the index.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
         final AppBuilder appBuilder = new AppBuilder();
         final JFrame application = appBuilder
-                // .addLoginView()
+                .addLoginView()
                 .addLoggedInView()
                 .addSelectPlaylistUseCase()
-                // .addLoginUseCase()
+                .addLoginUseCase()
                 .addAnalysisUseCase()
                 .addLogoutUseCase()
                 .build();

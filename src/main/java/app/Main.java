742bc07aa24baadf978c835954d485aff5d0778d
gcastalia <zhilin.gu@mail.utoronto.ca>
2025-12-02 04:54:02 -0500

    Complete Select Playlist Use Case and connect it to log in use case and analyze use case 2.modify necessary buttons in logged in view 3.implement propertychange() to support the changable status label
    (cherry picked from commit a1c89c57a741bd582ebac53d1606d8af9ee7cf90)

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,6 +13,7 @@ public class Main {
                 .addLoginView()
                 .addLoggedInView()
                 .addAnalysisView()
+                .addSelectPlaylistUseCase()
                 .addLoginUseCase()
                 .addAnalysisUseCase()
                 .addLogoutUseCase()
---
+++ src/main/java/use_case/select_playlist/SelectPlaylistInputBoundary.java
@@ -0,0 +1,6 @@
+package use_case.select_playlist;
+
+public interface SelectPlaylistInputBoundary {
+    void execute(SelectPlaylistInputData inputData);
+}
+
